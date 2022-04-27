cf22ad5355829542b62ac132051021a9f6dca778
rookie-leo <proleonarpa@gmail.com>
2022-04-26 23:27:30 -0300

    Variaveis globais, locais e estaticas

---
+++ basico/src/basico/Teste.java
@@ -0,0 +1,35 @@
+package basico;
+
+public class Teste {
+
+	int x = 3;
+	
+	public void m1() {
+		int x = 10;
+		System.out.println(x);
+	}
+	
+	public static void main(String[] x) {
+//		Teste teste = new Teste();
+//		teste.m1();
+//		
+//		Pessoa pessoa = new Pessoa();
+//		System.out.println(pessoa.id);
+//		System.out.println(Pessoa.id);
+//		Pessoa.m2();
+		
+		for (new Teste().x = 10; new Teste().x < 100; new Teste().x++) {
+			System.out.println(x);
+		}
+			
+	}
+	
+}
+
+class Pessoa {
+	static int id = 1;
+	
+	static void m2() {
+		System.out.println(id);
+	}
+}

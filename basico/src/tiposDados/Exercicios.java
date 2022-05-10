6ff8c006a7abf1cba73346fda6eec8dd5ad86093
rookie-leo <proleonarpa@gmail.com>
2022-05-10 12:05:11 -0300

    Corrigindo commit anterior: Precedencia de operadores com parenteses

--- basico/src/tiposDados/Exercicios.java
+++ basico/src/tiposDados/Exercicios.java
@@ -4,27 +4,27 @@ class Exercicios {
 
 	public static void main(String[] args) {
 
-//		int i;
-//		for (i = 0; i < 5; i++) {
-//			if (++i % 3 == 0) {
-//				break;
-//			}
-//		}
-//		System.out.println(i);
-//
-//		int $$ = 5;
-//		int __ = $$++;
-//		if (__ < ++$$ || __-- > $$)
-//			System.out.println("A");
-//		System.out.println($$);
-//		System.out.println(__);
-//
-//		char c = 65;
-//		char c2 = 68 - 65;
-//		System.out.println(c + c2);
-//
-//		String resultado = "Divisão dá " + 15 / 0.0;
-//		System.out.println(resultado);
+		int i;
+		for (i = 0; i < 5; i++) {
+			if (++i % 3 == 0) {
+				break;
+			}
+		}
+		System.out.println(i);
+
+		int $$ = 5;
+		int __ = $$++;
+		if (__ < ++$$ || __-- > $$)
+			System.out.println("A");
+		System.out.println($$);
+		System.out.println(__);
+
+		char c = 65;
+		char c2 = 68 - 65;
+		System.out.println(c + c2);
+
+		String resultado = "Divisão dá " + 15 / 0.0;
+		System.out.println(resultado);
 		
 		System.out.println(((!(true==false))==true ? 1 : 0)==0);
 	}

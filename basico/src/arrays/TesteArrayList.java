cb7bb7dc7efaf032b2a1586672119a8bf3e3f95a
rookie-leo <proleonarpa@gmail.com>
2022-05-12 18:37:33 -0300

    Iterator

---
+++ basico/src/arrays/TesteArrayList.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TesteArrayList {
+
+	public static void main(String... args) {
+		ArrayList<String> nomes = new ArrayList<>();
+		nomes.add("Java");
+		nomes.add("Ruby");
+		nomes.add("Scala");
+		
+		Iterator<String> iterator = nomes.iterator();
+		
+		while(iterator.hasNext()) {
+			String atual = iterator.next();
+			System.out.println(atual);
+		}
+	}
+	
+}

8d1ac41d9208cfe7207c2b8310d5bb69e1a110e1
rookie-leo <proleonarpa@gmail.com>
2022-05-13 11:39:37 -0300

    Exercicio laço for

--- basico/src/lacos/TestaFor.java
+++ basico/src/lacos/TestaFor.java
@@ -4,10 +4,14 @@ public class TestaFor {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0, j =0; j <= 10; System.out.println(i + "x" + j + "=" + i * j), i++, j++);
+//		for (int i = 0, j =0; j <= 10; System.out.println(i + "x" + j + "=" + i * j), i++, j++);
+//		
+//		
+//		for (int i = 0; i <= 10; i++) for (int j = 0; j <= 10; j++) System.out.println(i + "x" + j + "=" + i * j);
 		
-		
-		for (int i = 0; i <= 10; i++) for (int j = 0; j <= 10; j++) System.out.println(i + "x" + j + "=" + i * j);
+		for (int i = 0; i < 2; i++, System.out.println(i)) {
+			System.out.println(i);
+		}
 	}
 	
 }
